3e3b4daa7f44bfa935185392014fd60dbf4eca9b
paulviville <paul.viville@gmail.com>
2021-05-04 14:30:13 +0200

    edition base done

---
+++ MeshHandler.js
@@ -0,0 +1,455 @@
+import * as THREE from './CMapJS/Libs/three.module.js';
+import Renderer from './CMapJS/Rendering/Renderer.js';
+
+
+function MeshHandler (mesh, params = {}) {
+	const vertex = mesh.vertex;
+	const edge = mesh.edge;
+	const face = mesh.face;
+
+	const renderer = new Renderer(mesh);
+	const position = mesh.getAttribute(vertex, "position");
+	const savedPosition = mesh.addAttribute(vertex, "savedPosition");
+
+	const vertex_color = params.vertex_color || new THREE.Color(0xFF0000);
+	const vertex_select_color = params.vertex_select_color || new THREE.Color(0x00FF00);
+	const edge_color = params.edge_color || new THREE.Color(0x0000DD);
+	const edge_select_color = params.edge_select_color || new THREE.Color(0x00FF00);
+	const face_color = params.face_color || new THREE.Color(0x339999);
+	const face_select_color = params.face_select_color || new THREE.Color(0x00FF00);
+
+	const selectedVertices = new Set;
+	const selectedEdges = new Set;
+	const selectedFaces = new Set;
+	const selectionVertices = new Set;
+
+	let parentObject;
+	let verticesMesh, edgesMesh, facesMesh;
+	this.initialize = function (params = {}) {
+		if(params.vertices) {
+			renderer.vertices.create({size: 0.0015625 * 6, color: vertex_color}); 
+			verticesMesh = renderer.vertices.mesh;
+		}
+		if(params.edges) {
+			renderer.edges.create({size: 1.5, color: edge_color}); 
+			edgesMesh = renderer.edges.mesh;
+		}
+		if(params.faces) {
+			renderer.faces.create({color: face_color, side: THREE.DoubleSide}); 
+			facesMesh = renderer.faces.mesh;
+		}
+	};
+
+	this.addMeshesTo = function (parent) {
+		parentObject = parentObject || parent;
+		if(verticesMesh) renderer.vertices.addTo(parent);
+		if(edgesMesh) renderer.edges.addTo(parent);
+		if(facesMesh) renderer.faces.addTo(parent);
+	}
+
+
+	this.updateVertices = function() {
+		renderer.vertices.update();
+		verticesMesh = renderer.vertices.mesh;
+	};
+
+	this.updateEdges = function() {
+		renderer.edges.update();
+		edgesMesh = renderer.edges.mesh;
+	};
+
+	this.updateFaces = function() {
+		renderer.faces.update();
+		facesMesh = renderer.faces.mesh;
+	}
+
+	this.updateMeshes = function () {
+		if(verticesMesh) {
+			this.updateVertices();
+		}
+		if(edgesMesh) {
+			this.updateEdges();
+		}
+		if(facesMesh) {
+			this.updateFaces();
+		}
+	}
+
+	function raycast (raycaster, target) {
+		return raycaster.intersectObject(target)[0];
+	};
+
+	this.positionHit = function (raycaster) {
+		let targets = [];
+		if(verticesMesh) targets.push(verticesMesh);
+		if(edgesMesh) targets.push(edgesMesh);
+		if(facesMesh) targets.push(facesMesh);
+		console.log(facesMesh)
+		const hit = raycaster.intersectObjects(targets);
+		return (hit[0] ? hit[0].point : undefined);
+	};
+
+	this.edgePoint = function (eid, point) {
+		let e0 = edgesMesh.ed[eid];
+		let projection = new THREE.Vector3;
+		let A = new THREE.Vector3;
+		let B = new THREE.Vector3;
+		let vs = [];
+		mesh.foreachIncident(vertex, edge, e0, v => {
+			vs.push(v);
+		});
+		B.subVectors(position[vs[1]], position[vs[0]]);
+		A.subVectors(point, position[vs[0]]);
+		B.multiplyScalar(A.dot(B) / B.dot(B));
+		projection.add(B).add(position[vs[0]]);
+		return projection;
+	};
+
+	this.selectHit = function (raycaster, params) {
+		params = params ? params : {vertices: true, edges: true, faces: true};
+		let vertexHit = (verticesMesh && params.vertices)? raycast(raycaster, verticesMesh) : undefined;
+		if(vertexHit) {
+			let vid = vertexHit.instanceId;
+			selectedVertices.add(vid);
+			this.changeVertexColor(vid, vertex_select_color);
+			return vertexHit;
+		}
+		let edgeHit = (edgesMesh && params.edges) ? raycast(raycaster, edgesMesh): undefined;
+		if(edgeHit) {
+			let eid = edgeHit.instanceId;
+			selectedEdges.add(eid);
+			this.changeEdgeColor(eid, edge_select_color);
+			return edgeHit;
+		}
+		let faceHit = (facesMesh && params.faces) ? raycast(raycaster, facesMesh) : undefined;
+		if(faceHit) {
+			let fid = faceHit.faceIndex;
+			selectedFaces.add(fid);
+			this.changeFaceColor(fid, edge_select_color);
+			return faceHit;
+		}
+
+		return undefined;
+	};
+
+	this.toggleSelectHit = function (raycaster, params) {
+		params = params ? params : {vertices: true, edges: true, faces: true};
+		let vertexHit = (verticesMesh && params.vertices)? raycast(raycaster, verticesMesh) : undefined;
+		if(vertexHit) {
+			let vid = vertexHit.instanceId;
+			if(selectedVertices.has(vid)) {
+				selectedVertices.delete(vid);
+				this.changeVertexColor(vid, vertex_color);
+			}
+			else {
+				selectedVertices.add(vid);
+				this.changeVertexColor(vid, vertex_select_color);
+			}
+			return vertexHit;
+		}
+		let edgeHit = (edgesMesh && params.edges) ? raycast(raycaster, edgesMesh): undefined;
+		if(edgeHit) {
+			let eid = edgeHit.instanceId;
+			if(selectedEdges.has(eid)) {
+				selectedEdges.delete(eid);
+				this.changeEdgeColor(eid, edge_color);
+			}
+			else {
+				selectedEdges.add(eid);
+				this.changeEdgeColor(eid, edge_select_color);
+			}
+			return edgeHit;
+		}
+		let faceHit = (facesMesh && params.faces) ? raycast(raycaster, facesMesh) : undefined;
+		if(faceHit) {
+			let fid = faceHit.faceIndex;
+			if(selectedFaces.has(fid)) {
+				selectedFaces.delete(fid);
+				this.changeFaceColor(fid, face_color);
+			}
+			else {
+				selectedFaces.add(fid);
+				this.changeFaceColor(fid, face_select_color);
+			}
+			return faceHit;
+		}
+
+		return undefined;
+	};
+
+	this.deselectAll = function (params) {
+		params = params ? params : {vertices: true, edges: true, faces: true};
+		if(params.vertices) {
+			selectedVertices.forEach(v => this.changeVertexColor(v, vertex_color));
+			selectedVertices.clear();
+		}
+
+		if(params.edges) {
+			selectedEdges.forEach(e => this.changeEdgeColor(e, edge_color));
+			selectedEdges.clear();
+		}
+
+		if(params.faces) {
+			selectedFaces.forEach(f => this.changeFaceColor(f, face_color));
+			selectedFaces.clear();
+		}
+	};
+
+	this.hasSelection = function (params) {
+		params = params ? params : {vertices: true, edges: true, faces: true};
+		let hasSelection = 0;
+		hasSelection += (params.vertices ? selectedVertices.size : 0);
+		hasSelection += (params.edges ? selectedEdges.size : 0);
+		hasSelection += (params.faces ? selectedFaces.size : 0);
+		return hasSelection;
+	}
+
+	this.selectVertex = function (vid) {
+		selectedVertices.add(vid);
+		this.changeVertexColor(vid, vertex_select_color);
+	};
+
+	this.deselectVertex = function (vid) {
+		selectedVertices.delete(vid);
+		this.changeVertexColor(vid, vertex_color);
+	};
+
+	this.selectEdge = function (eid) {
+		selectedEdges.add(eid);
+		this.changeEdgeColor(eid, edge_select_color);
+	};
+
+	this.deselectEdge = function (eid) {
+		selectedEdges.delete(eid);
+		this.changeEdgeColor(eid, edge_color);
+	};
+
+	this.changeVertexColor = function (vid, color) {
+		verticesMesh.setColorAt(vid, color);
+		renderer.vertices.mesh.instanceColor.needsUpdate = true;
+	};
+
+	this.changeEdgeColor = function (eid, color) {
+		edgesMesh.setColorAt(eid, color);
+		edgesMesh.instanceColor.needsUpdate = true;
+	};
+
+	this.changeFaceColor = function (fid, color) {
+		facesMesh.geometry.faces[fid].color.copy(color);
+		facesMesh.geometry.colorsNeedUpdate = true;
+	};
+
+	this.addFaceFromSelection = function () {
+		if(this.hasSelection({edges: true})) {
+			const edges = Array.from(selectedEdges).map(eid => edgesMesh.ed[eid]);
+			if(mesh.addFace(...edges) != undefined) {
+				this.updateFaces();
+				this.deselectAll({edges: true});
+			}
+		}
+	};
+
+	this.addEdgeFromSelection = function () {
+		if(this.hasSelection({vertices: true}) == 2) {
+			let vertices = Array.from(selectedVertices);
+			let v0 = verticesMesh.vd[vertices[0]];
+			let v1 = verticesMesh.vd[vertices[1]];
+			let ed = mesh.addEdge(v0, v1);
+			this.updateEdges();
+			this.deselectAll({vertices: true});
+			return ed;
+		}
+	};
+
+	this.addVertex = function (pos) {
+		let v = mesh.addVertex();
+		position[v] = pos.clone();
+		this.updateVertices();
+		let vid = mesh.getAttribute(vertex, "instanceId")[v];
+		return vid;
+	};
+
+	this.extrudeEdge = function (eid) {
+		let e0 = edgesMesh.ed[eid];
+		let e0v = [];
+		let e1v = [];
+		let es = [];
+		mesh.foreachIncident(vertex, edge, e0, v0 => {
+			e0v.push(v0);
+			let v1 = mesh.addVertex();
+			e1v.push(v1);
+			position[v1] = position[v0].clone();
+			es.push(mesh.addEdge(v0, v1));
+		});
+
+		let e1 = mesh.addEdge(e1v[0], e1v[1]);
+		mesh.addFace(e0, es[0], e1, es[1]);
+
+		this.updateVertices();
+		this.updateEdges();
+		this.updateFaces();
+
+		return mesh.getAttribute(edge, "instanceId")[e1];
+	};
+
+	this.cutEdge = function (eid, point) {
+		console.log(eid, point, edgesMesh.ed[eid])
+		let p = this.edgePoint(eid, point)
+		let v = mesh.cutEdge(edgesMesh.ed[eid]);
+		position[v] = p;
+		this.updateVertices();
+		this.updateEdges();
+
+		let vid = mesh.getAttribute(vertex, "instanceId")[v];
+		mesh.debug();
+		console.log(position)
+		return vid;
+	};
+
+	this.cutFace = function () {
+		let it = selectedVertices.entries();
+		const v0 = it.next().value[0];
+		const v1 = it.next().value[0];
+
+		const faces0 = [];
+		mesh.foreachIncident(face, vertex, v0, f => faces0.push(f));
+
+		const faces1 = [];
+		mesh.foreachIncident(face, vertex, v1, f => faces1.push(f));
+
+		const commonFace = faces0.filter(f => faces1.includes(f));
+		if (commonFace.length) {
+			mesh.cutFace(commonFace[0], v0, v1);
+			this.updateEdges();
+			this.updateFaces();
+		}	
+	};
+
+	// TODO: mesh.cell(vertex, v) for cmap compatibility
+	this.saveSelectionPosition = function () {
+		selectionVertices.clear();
+
+		selectedVertices.forEach(vid => {
+			selectionVertices.add(verticesMesh.vd[vid]);
+		});
+
+		selectedEdges.forEach(eid => {
+			let e = edgesMesh.ed[eid];
+			mesh.foreachIncident(vertex, edge, e, v => {
+				selectionVertices.add(v);
+			});
+		});
+
+		selectedFaces.forEach(fid => {
+			let f = facesMesh.fd[fid];
+			mesh.foreachIncident(vertex, face, f, v => {
+				selectionVertices.add(v);
+			});
+		});
+
+		selectionVertices.forEach(v => {
+			savedPosition[v] = position[v].clone();
+
+		});
+	};
+
+	this.displaceSelection = function (delta) {
+		selectionVertices.forEach(vd => {
+			position[vd].addVectors(savedPosition[vd], delta);
+		});
+		this.updateVertices();
+		this.updateEdges();
+		this.updateFaces();
+		this.recolorSelection();
+	};
+
+	this.cancelSelectionMove = function () {
+		selectionVertices.forEach(vd => {
+			position[vd].copy(savedPosition[vd]);
+		});
+		this.updateVertices();
+		this.updateEdges();
+		this.updateFaces();
+	};
+
+	this.targetCenter = function (raycaster) {}
+
+	this.getPosition = function (v) {};
+
+	this.getEdgeMid = function (e) {};
+
+	this.getFaceCenter = function (f) {};
+
+	this.saveVertexPos = function (v) {};
+	this.savedVertexPos = function (v) {};
+	this.updateVertex = function (v) {};
+	this.updateEdge = function (e) {};
+
+	// this.rescale_vertices
+
+	this.recolorSelection = function () {
+		if(selectedFaces.size) {
+			selectedFaces.forEach(fid => {
+				this.changeFaceColor(fid, face_select_color);
+			});
+		}
+
+		if(selectedEdges.size) {
+			selectedEdges.forEach(eid => {
+				this.changeEdgeColor(eid, edge_select_color);
+			});
+		}
+
+		if(selectedVertices.size) {
+			selectedVertices.forEach(vid => {
+				this.changeVertexColor(vid, vertex_select_color);	
+			});
+		}
+	}
+
+	this.deleteSelection = function () {
+		const update = {};
+		if(selectedFaces.size) {
+			selectedFaces.forEach(fid => {
+				let f = facesMesh.fd[fid];
+				mesh.deleteFace(f);
+			});
+			selectedFaces.clear();
+			update.faces = true;
+		}
+
+		if(selectedEdges.size) {
+			selectedEdges.forEach(eid => {
+				let e = edgesMesh.ed[eid];
+				mesh.deleteEdge(e);
+			});
+			selectedEdges.clear();
+			update.edges = true;
+			update.faces = true;
+		}
+
+		if(selectedVertices.size) {
+			selectedVertices.forEach(vid => {
+				let v = verticesMesh.vd[vid];
+				mesh.deleteVertex(v);
+			});
+			selectedVertices.clear();
+			update.vertices = true;
+			update.edges = true;
+			update.faces = true;
+		}
+
+		update.vertices ? this.updateVertices() : undefined;
+		update.edges ? this.updateEdges() : undefined;
+		update.faces ? this.updateFaces() : undefined;
+		this.recolorSelection();
+	};
+
+	this.delete = function () {
+		renderer.vertices.delete();
+		renderer.edges.delete();
+		renderer.faces.delete();
+	}
+}
+
+export default MeshHandler
---
+++ main.js
@@ -0,0 +1,524 @@
+import {CMap0} from './CMapJS/CMap/CMap.js';
+import {CMap1} from './CMapJS/CMap/CMap.js';
+import {CMap2} from './CMapJS/CMap/CMap.js';
+import {CMap3} from './CMapJS/CMap/CMap.js';
+import IncidenceGraph from './CMapJS/CMap/IncidenceGraph.js';
+import Renderer from './CMapJS/Rendering/Renderer.js';
+import * as THREE from './CMapJS/Libs/three.module.js';
+import { OrbitControls } from './CMapJS/Libs/OrbitsControls.js';
+import { loadCMap2 } from './CMapJS/IO/Surface_Formats/CMap2IO.js';
+import { loadSurfaceView } from './CMapJS/Rendering/displayOnly.js';
+import { controllers, GUI } from './CMapJS/Libs/dat.gui.module.js';
+
+import { TransformControls } from './CMapJS/Libs/TransformControls.js'
+import MeshHandler from './MeshHandler.js';
+import Gizmo from './Gizmo.js';
+
+
+import {importIncidenceGraph, exportIncidenceGraph} from './CMapJS/IO/IncidenceGraphFormats/IncidenceGraphIO.js';
+import {test0_ig} from './ig_files.js';
+
+let ig = importIncidenceGraph("ig", test0_ig);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xAAAAAA);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000.0);
+camera.position.set(0, 0, 2);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+window.addEventListener('resize', function() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
+
+
+
+let ambientLight = new THREE.AmbientLight(0xAAAAFF, 0.5);
+scene.add(ambientLight);
+let pointLight0 = new THREE.PointLight(0x3137DD, 5);
+pointLight0.position.set(10,8,5);
+scene.add(pointLight0);
+
+
+
+let map_handler = new MeshHandler(ig);
+map_handler.initialize({vertices: true, edges: true, faces: true});
+map_handler.addMeshesTo(scene);
+let surfaceView = null;
+let gizmo = new Gizmo();
+	gizmo.initialize();
+	gizmo.addTo(scene);
+
+const event_handler = new (function(scope, map_handler){
+	function Mode(start, stop) {
+		let on = false;
+		this.start = function() {
+			start();
+			on = true;
+			return true;
+		};
+		this.stop = function(){
+			stop();
+			on = false;
+		};
+	
+		this.toggle = function() {
+			on ? this.stop() : this.start();
+		}
+	}	
+
+	const orbit_controls = new OrbitControls(camera, scope)
+	orbit_controls.enablePan = false;
+	orbit_controls.mouseButtons.MIDDLE = THREE.MOUSE.ROTATE;
+	orbit_controls.mouseButtons.LEFT = null;
+	orbit_controls.mouseButtons.RIGHT = null;
+	
+	
+	
+
+	const raycaster = new THREE.Raycaster;
+	const mouse = new THREE.Vector2;
+	
+	function setMouse(event) {
+		mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+		mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+	}
+
+	let activeMode = undefined;
+	const keyHeld = {};
+
+	let vertex = null;
+	let edge = null;
+
+	const selectMouseDown = function(event) {
+		setMouse(event);
+		if(event.button == 0){
+			raycaster.setFromCamera(mouse, camera);
+			if(!keyHeld.ShiftLeft)
+				map_handler.deselectAll();
+
+			let hit = keyHeld.ShiftLeft ? map_handler.toggleSelectHit(raycaster) : map_handler.selectHit(raycaster);
+		}
+	}
+
+	const modeSelect = new Mode(
+		() => {
+			scope.addEventListener( 'pointerdown', selectMouseDown );
+		},
+		() => {
+			scope.removeEventListener( 'pointerdown', selectMouseDown );
+		}
+	);
+
+	const OPdblClick = function(event) {
+		setMouse(event);
+		raycaster.setFromCamera(mouse, camera);
+		let point = map_handler.positionHit(raycaster);
+		if(point) {
+			orbit_controls.target.copy(point)
+			orbit_controls.update();
+		}
+	}
+
+	const modeOrbit = new Mode(
+		() => {
+			orbit_controls.mouseButtons.LEFT = THREE.MOUSE.ROTATE;
+			scope.addEventListener('dblclick', OPdblClick, false);
+		},
+		() => {
+			scope.removeEventListener('dblclick', OPdblClick, false);
+			orbit_controls.mouseButtons.LEFT = null;
+		}
+	);
+
+	const modePan = new Mode(
+		() => {
+			scope.addEventListener('dblclick', OPdblClick);
+			orbit_controls.mouseButtons.LEFT = THREE.MOUSE.PAN;
+			orbit_controls.enablePan = true;
+		},
+		() => {
+			scope.removeEventListener('dblclick', OPdblClick);
+			orbit_controls.mouseButtons.LEFT = null;
+			orbit_controls.enablePan = false;
+		}
+	);
+
+
+	let sphere = new THREE.Mesh(new THREE.SphereBufferGeometry(0.01, 10, 10), new THREE.MeshLambertMaterial({color: 0x0000FF}));
+	let sphereDelta = new THREE.Mesh(new THREE.SphereBufferGeometry(0.01, 10, 10), new THREE.MeshLambertMaterial({color: 0x00FF00}));
+	let sphereEdge = new THREE.Mesh(new THREE.SphereBufferGeometry(0.01, 10, 10), new THREE.MeshLambertMaterial({color: 0xFFFF00}));
+	scene.add(sphereEdge);
+	const getGizmoConstraint = function () {
+		if(keyHeld.Digit1)
+			return keyHeld.ShiftLeft ? gizmo.constrain.X : gizmo.constrain.YZ;
+
+		if(keyHeld.Digit2)
+			return keyHeld.ShiftLeft ? gizmo.constrain.Y : gizmo.constrain.XZ;
+
+		if(keyHeld.Digit3)
+			{
+				console.log(gizmo.constrain.Z);
+				return keyHeld.ShiftLeft ? gizmo.constrain.Z : gizmo.constrain.XY;}
+	}
+
+	const worldPos0 = new THREE.Vector3;
+	const delta = new THREE.Vector3;
+
+	const moveMouseMove = function(event) {
+		setMouse(event);
+		raycaster.setFromCamera(mouse, camera);
+		let p = gizmo.positionHit(raycaster, getGizmoConstraint());
+		console.log(delta)
+		delta.subVectors(p, worldPos0);
+		sphere.position.copy(p);
+		map_handler.displaceSelection(delta);
+	}
+
+	const moveMouseUp = function(event) {
+		scope.removeEventListener( 'pointermove', moveMouseMove );
+		scope.removeEventListener( 'pointerup', moveMouseUp );
+	}
+
+	const moveMouseDown = function(event) {
+		setMouse(event);
+		if(!map_handler.hasSelection())
+			selectMouseDown(event);
+
+		if(event.button == 0 && map_handler.hasSelection()){
+			map_handler.saveSelectionPosition();
+			raycaster.setFromCamera(mouse, camera);
+			let hit = map_handler.positionHit(raycaster);
+			if(hit){
+				gizmo.setPosition(hit);
+				gizmo.update(camera);
+				worldPos0.copy(gizmo.position);
+			}
+			else
+				worldPos0.copy(gizmo.positionHit(raycaster, getGizmoConstraint()));
+			
+			sphere.position.copy(worldPos0);
+			sphereDelta.position.copy(worldPos0);
+			scope.addEventListener( 'pointermove', moveMouseMove );
+			scope.addEventListener( 'pointerup', moveMouseUp );
+			
+		}
+	}
+
+	const modeMove = new Mode(
+		() => {
+			scene.add(sphere);
+			scene.add(sphereDelta);
+			scope.addEventListener( 'pointerdown', moveMouseDown );
+		},
+		() => {
+			scene.remove(sphere);
+			scene.remove(sphereDelta);
+			scope.removeEventListener( 'pointerdown', moveMouseDown );
+		}
+	);
+
+	const addFaceSelectMouseDown = function(event) {
+		setMouse(event);
+		if(event.button == 0){
+			raycaster.setFromCamera(mouse, camera);
+
+			map_handler.toggleSelectHit(raycaster, {edges: true});
+			map_handler.addFaceFromSelection();
+		}
+	}
+
+	const modeAddFace = new Mode(
+		() => {
+			map_handler.deselectAll({vertices: true, faces:  true});
+			map_handler.addFaceFromSelection();
+			scope.addEventListener( 'pointerdown', addFaceSelectMouseDown );
+		},
+		() => {
+			scope.removeEventListener( 'pointerdown', addFaceSelectMouseDown );
+		}
+	);
+
+	const addEdgeSelectMouseDown = function(event) {
+		setMouse(event);
+		if(event.button == 0){
+			raycaster.setFromCamera(mouse, camera);
+
+			map_handler.toggleSelectHit(raycaster, {vertices: true});
+			if(map_handler.hasSelection({vertices: true}) == 2){
+				map_handler.addEdgeFromSelection();
+			}
+		}
+	}
+
+	const modeAddEdge = new Mode(
+		() => {
+			map_handler.deselectAll();
+			scope.addEventListener( 'pointerdown', addEdgeSelectMouseDown );
+		},
+		() => {
+			scope.removeEventListener( 'pointerdown', addEdgeSelectMouseDown );
+		}
+	);
+	
+	const extrudeMouseDown = function(event) {
+		setMouse(event);
+		map_handler.deselectAll();
+		if(event.button == 0){
+			raycaster.setFromCamera(mouse, camera);
+
+			let vertexHit = map_handler.selectHit(raycaster, {vertices: true});
+			if(vertexHit) {
+				let vid0 = vertexHit.instanceId;
+				console.log(vertexHit)
+				let vid1 = map_handler.addVertex(vertexHit.point);
+				map_handler.selectVertex(vid1);
+				map_handler.addEdgeFromSelection();
+				map_handler.selectVertex(vid1);
+				moveMouseDown(event);
+				return;
+			}
+
+			let edgeHit = map_handler.selectHit(raycaster, {edges: true});
+			if(edgeHit) {
+				let eid0 = edgeHit.instanceId;
+				let eid1 = map_handler.extrudeEdge(eid0);
+				map_handler.deselectEdge(eid0);
+				map_handler.deselectAll();
+				map_handler.selectEdge(eid1);
+				moveMouseDown(event);
+
+			}
+		}
+	}
+	const modeExtrude = new Mode(
+		() => {
+			map_handler.deselectAll();
+			scope.addEventListener( 'pointerdown', extrudeMouseDown );
+		},
+		() => {
+			scope.removeEventListener( 'pointerdown', extrudeMouseDown );
+		},
+	);
+
+	const cutEdgeMouseDown = function(event) {
+		setMouse(event);
+		map_handler.deselectAll();
+		if(event.button == 0){
+			raycaster.setFromCamera(mouse, camera);
+
+			let edgeHit = map_handler.selectHit(raycaster, {edges: true});
+			if(edgeHit) {
+				let eid0 = edgeHit.instanceId;
+				let vid = map_handler.cutEdge(eid0, edgeHit.point)
+				map_handler.deselectAll();
+				map_handler.selectVertex(vid);
+				moveMouseDown(event);
+				// sphereEdge.position.copy(map_handler.edgePoint(edgeHit.point, eid0));
+			}
+		}
+	}	
+	const modeCutEdge = new Mode(
+		() => {
+			map_handler.deselectAll();
+			scope.addEventListener( 'pointerdown', cutEdgeMouseDown );
+		},
+		() => {
+			scope.removeEventListener( 'pointerdown', cutEdgeMouseDown );
+		},
+	);
+
+	const cutFaceMouseDown = function(event) {
+		setMouse(event);
+		// map_handler.deselectAll();
+		if(event.button == 0){
+			raycaster.setFromCamera(mouse, camera);
+
+			let vertexHit = map_handler.selectHit(raycaster, {vertices: true});
+			if(vertexHit) {
+				let vid = vertexHit.instanceId;
+				map_handler.selectVertex(vid);
+				if(map_handler.hasSelection({vertices: true}) == 2) {
+					map_handler.cutFace();
+					map_handler.deselectAll();
+				}
+				return;
+			}
+		}
+	}
+	const modecutFace = new Mode(
+		() => {
+			map_handler.deselectAll();
+			console.log("cut face");
+			scope.addEventListener( 'pointerdown', cutFaceMouseDown );
+		},
+		() => {
+			scope.removeEventListener( 'pointerdown', cutFaceMouseDown );
+		},
+	);
+
+	const defaultKeyDown = function(event){
+		keyHeld[event.code] = true;
+	};
+
+	const defaultKeyUp = function(event){
+		console.log(event.which, event.code, event.charCode);
+		let nextMode = undefined;
+		switch(event.code) {
+			case "Escape": // deselect
+				map_handler.deselectAll();
+				break;
+			case "Space": // select mode
+				nextMode = modeSelect;
+				break;
+			case "Delete": // delete selection
+				map_handler.deleteSelection();
+				break;
+			case "KeyA": // add vertices mode
+
+				break;
+			case "KeyC": // cut edge
+				nextMode = modeCutEdge;
+				break;
+			case "KeyE": // extrude
+				nextMode = modeExtrude;
+				break;
+			case "KeyF": // create face
+				nextMode = modeAddFace;
+				break
+			case "KeyL": // draw
+				nextMode = modeAddEdge;
+				break;
+			case "Semicolon": // azerty -> "m" move
+			case "KeyM":
+				nextMode = modeMove;
+				break;
+			case "KeyO": // orbit
+				nextMode = modeOrbit;
+				break;
+			case "KeyP": // pan
+				nextMode = modePan;
+				break;
+			case "KeyX": // Cut face
+				nextMode = modecutFace;
+				break;
+		};
+
+		if(nextMode) {
+			if(activeMode) 
+				activeMode.stop();
+			activeMode = nextMode;
+			activeMode.start();
+		}
+
+		keyHeld[event.code] = false;
+
+	}
+
+	scope.addEventListener("keydown", defaultKeyDown);
+	scope.addEventListener("keyup", defaultKeyUp);
+	activeMode = modeSelect;
+	activeMode.start();
+
+
+	function load(blob)
+	{
+		let file_name = blob.name;
+		let reader = new FileReader();
+		return new Promise( (resolve, reject) =>
+		{
+			reader.onerror = () => 
+			{
+				reader.abort();
+				ewgl_common.console.error('can not load '+blob.name);
+				reject();
+			};
+			reader.onload = () => 
+			{
+				resolve(reader.result);
+			};
+			reader.readAsText(blob);
+		});
+	}
+
+	function FileDroppedOnCanevas(func)
+	{
+		scope.addEventListener("dragenter", e =>
+		{
+			e.stopPropagation();
+			e.preventDefault();
+		}, false);
+
+		scope.addEventListener("dragover",  e =>
+		{
+			e.stopPropagation();
+			e.preventDefault();
+		}, false);
+
+		scope.addEventListener("drop", e =>
+		{
+			e.stopPropagation();
+			e.preventDefault();
+			const dt = e.dataTransfer;
+			const files = dt.files;
+			func(files[0]);
+		}, false);
+	}
+	
+	FileDroppedOnCanevas( (blob) =>
+	{
+		load(blob).then((meshFile) =>
+		{
+			console.log(meshFile);
+			if(blob.name.match(/.cg/))
+			{
+				
+			}
+			if(blob.name.match(/.ig/))
+			{
+				map_handler.delete();
+				map_handler = new MeshHandler(importIncidenceGraph("ig", meshFile));
+				map_handler.initialize({vertices: true, edges: true, faces: true});
+				map_handler.addMeshesTo(scene);
+			}
+			if(blob.name.match(/.off/))
+			{
+				if(surfaceView)
+					scene.remove(surfaceView);
+				surfaceView = loadSurfaceView("off", meshFile, {transparent: true, opacity: 0.8});
+				scene.add(surfaceView);
+			}
+		});
+	});
+
+	return this;
+})(renderer.domElement, map_handler);
+
+window.event_handler = event_handler;
+window.map_handler = map_handler;
+window.ig = ig;
+
+
+function update ()
+{
+	gizmo.update(camera);
+}
+
+function render()
+{
+	renderer.render(scene, camera);
+}
+
+function mainloop()
+{
+    update();
+    render();
+    requestAnimationFrame(mainloop);
+}
+
+mainloop();
